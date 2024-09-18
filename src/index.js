2609d58bf42300de3bbdd1daf6526ab3d4413382
aggy2024 <agnessmakoni@gmail.com>
2024-09-18 16:19:11 +0200

    deletion

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = 
+
     citiesElement.innerHTML = `
   <div class="city">
     <div>
