ce6d7486d77c86dad642ae15a096dd0c6d1067cd
aggy2024 <agnessmakoni@gmail.com>
2024-09-18 16:11:02 +0200

    corrected index.js

--- src/index.js
+++ src/index.js
@@ -28,17 +28,18 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = 
-    <div class = "city">
-      <div>
-    <h2>${cityName}</h2>
-  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
   </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-      "A"
-    )}</small></div>
-  </div>
-  <a href = "/">All cities</a>
-  ;
+  <a href="/">All cities</a>
+  `; 
 }
 updateTime();
 setinterval(updateTime, 1000);
